9c8b4e76dd02c0930b02596c995ff65100c8307b
Rémi Cocula <rcocula@free.fr>
2020-11-13 19:11:42 +0100

    WIP

--- compose.sh
+++ compose.sh
@@ -4,8 +4,8 @@
 #export SOUNDCLOUD_KEY="3-35204-109597253-rzbwQuGn9AgsJSs"
 # clé Rémi
 export SOUNDCLOUD_KEY="1-35204-62929422-d2896e1de186351"
-
-echo ${SOUNDCLOUD_KEY}
+export HOSTNAME=`hostname`
+export MUSIC_DIR="/home/remi/Soulseek Downloads/complete/"
 
 docker-compose up -d --build
 
