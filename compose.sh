c4ea503719f3790021346124066ed49987334831
remi <rcocula@gmail.com>
2021-01-17 17:09:18 +0100

    deploy bananapi

--- compose.sh
+++ compose.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# clé Véro
-#export SOUNDCLOUD_KEY="3-35204-109597253-rzbwQuGn9AgsJSs"
-# clé Rémi
-export SOUNDCLOUD_KEY="1-35204-62929422-d2896e1de186351"
 export HOSTNAME=`hostname`
-export MUSIC_DIR="/mnt/DATA/REMI/musique/PCDM/"
 
-docker-compose up -d --build
+docker-compose down -v
+
+docker-compose --env-file .env.${HOSTNAME} up -d --build
 
---
+++ mopidy/scan.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mopidy local scan > /var/lib/mopidy/local/local-scan.log 2>&1
+
+
